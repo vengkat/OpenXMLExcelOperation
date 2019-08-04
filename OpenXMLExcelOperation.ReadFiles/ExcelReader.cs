389a00244bb72bcb9a2c3a987bdbb15e2d8f7fb1
vengkat <v.vengkat@gmail.com>
2019-08-04 19:45:47 +0530

    initial commit

---
+++ OpenXMLExcelOperation.ReadFiles/ExcelReader.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+
+namespace OpenXMLExcelOperation.ReadFiles
+{
+    public class ExcelReader
+    {
+        #region ReadFileWithText
+        /// <summary>
+        /// ReadFileWithText - Reads the values of each cell in an excel file
+        /// </summary>
+        /// <param name="fileName"></param>
+        public void ReadFileWithText(string fileName)
+        {
+            //string fileName = @"C:\Files\Test.xlsx";
+
+            using (FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                using (SpreadsheetDocument doc = SpreadsheetDocument.Open(fs, false))
+                {
+                    WorkbookPart workbook                 = doc.WorkbookPart;
+                    SharedStringTablePart StringTablePart = workbook.GetPartsOfType<SharedStringTablePart>().First();
+                    SharedStringTable stringTable         = StringTablePart.SharedStringTable;
+
+                    WorksheetPart worksheetPart = workbook.WorksheetParts.First();
+                    Worksheet workSheet         = worksheetPart.Worksheet;
+
+                    var rows = workSheet.Descendants<Row>();
+
+                    Console.WriteLine($"Row count = {rows.LongCount()}");
+
+                    // Loop through each row in the workSheet
+                    foreach (Row row in rows)
+                    {
+                        foreach (Cell cell in row.Elements<Cell>())
+                        {
+                            // Loop through each cell in the row
+                            if (cell.DataType != null)//For number values cell.DataType appears to be null
+                            {
+                                try
+                                {
+                                    int cellId = int.Parse(cell.CellValue.Text);
+                                    string value = stringTable.ChildElements[cellId].InnerText;
+                                    //This prints the text values in the cell
+                                    Console.WriteLine($"Cell value: {value}");
+                                }
+                                catch (Exception ex)
+                                {
+                                    Console.WriteLine($"Internal server error | {ex.Message}");
+                                }
+                            }
+                            else
+                            {                     
+                                //This prints the numeric values in the cell
+                                Console.WriteLine($"Cell value: {cell.CellValue?.Text}");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        #endregion
+    }
+}
---
+++ OpenXMLExcelOperation.ReadFiles/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenXMLExcelOperation.ReadFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName    = @"C:\Files\Test.xlsx";
+            //Or read file path from console
+            if (File.Exists(fileName))
+            {
+                try
+                {
+                    ExcelReader reader = new ExcelReader();
+                    reader.ReadFileWithText(fileName);
+                    Console.ReadLine();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Internal server error | {ex.Message}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid file path specified!");
+            }
+        }
+    }
+}
